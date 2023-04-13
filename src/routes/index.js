3224fb024eea7cd945c58b6697308b0a00557497
Hasan Raza <hasan.raza@blackfrosttechnologies.com>
2023-04-13 17:40:22 -0400

    feat: added GET /api/warehouse route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 import { Router } from 'express'
+import warehouseRouter from '#app/routes/warehouse.js'
 
 const routes = Router()
 
 // Routing middleware
 
+
 // All possible routes
+routes.use('/api/warehouse', warehouseRouter)
+
 
 export default routes
