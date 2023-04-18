dfd39b6256b353dce70fc2fc69791ca836b5833c
A-Trott <100259248+A-Trott@users.noreply.github.com>
2023-04-18 14:20:09 -0600

    Add inventory item routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import warehouseRouter from '#app/routes/warehouse.js'
+import inventoryRouter from '#app/routes/inventories.js'
 
 const routes = Router()
 
@@ -8,6 +9,7 @@ const routes = Router()
 
 // All possible routes
 routes.use('/api/warehouse', warehouseRouter)
+routes.use('/api/inventories', inventoryRouter)
 
 
 export default routes
--- src/routes/warehouse.js
+++ src/routes/warehouse.js
@@ -5,14 +5,10 @@ const warehouseRouter = Router()
 
 warehouseRouter
     .get('/', WarehouseController.listAll)
-<<<<<<< HEAD
     .post('/', WarehouseController.addWarehouse)
     .get('/:id', WarehouseController.listById)
     .delete('/:id', WarehouseController.deleteById)
     .put('/:id', WarehouseController.updateById)
 
-=======
-    .get('/:id', WarehouseController.listById)
->>>>>>> 0bd86d4 (feat(PTSEP3-15): added GET /api/warehouse/:id route)
 
 export default warehouseRouter
