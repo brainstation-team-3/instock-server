1bfab544ca40a9c71d1e3158ddb3d4a2938ce28c
Kaveri Mittal <kaverimittal@Shivams-MacBook-Air.local>
2023-04-17 22:46:51 -0400

    changes accepted

--- src/models/warehouse.js
+++ src/models/warehouse.js
@@ -4,15 +4,9 @@ class Warehouse {
     listAll() {
         return database
             .from('warehouse')
-            .select(
-                'id',
-                'warehouse_name',
-                'address',
-                'city',
-                'country',
-                'contact_name',
-                'contact_phone',
-                'contact_email'
+            .select('id', 'warehouse_name', 'address',
+                'city', 'country', 'contact_name',
+                'contact_phone', 'contact_email',
             )
     }
 
@@ -22,43 +16,11 @@ class Warehouse {
             .first()
             .from('warehouse')
             .where('id', id)
-            .select(
-                'id',
-                'warehouse_name',
-                'address',
-                'city',
-                'country',
-                'contact_name',
-                'contact_phone',
-                'contact_email'
+            .select('id', 'warehouse_name', 'address',
+                'city', 'country', 'contact_name',
+                'contact_phone', 'contact_email',
             )
     }
-
-    addWarehouse() {
-        return database
-            .insert(req.body)
-            .returning('*')
-            .into('warehouse')
-            .then(function(data) {
-                res.send(data)
-            })
-    }
-
-
-    async deleteById(id) {
-        const warehouse = await database
-            .from('warehouse')
-            .where('id', id)
-            .del()
-
-        const inventory = await database
-            .from('inventory')
-            .where('warehouse_id', id)
-            .del()
-
-        return id
-    }
-
 }
 
 export default new Warehouse()
