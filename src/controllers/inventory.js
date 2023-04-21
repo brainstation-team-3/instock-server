a3a282017bfc0653e568093c93ee6ee4db0df607
Kaveri Mittal <kaverimittal@Shivams-MacBook-Air.local>
2023-04-20 20:25:00 -0400

    feat: API to GET List of all Inventory Items, API to DELETE an Inventory Item

--- src/controllers/inventory.js
+++ src/controllers/inventory.js
@@ -1,25 +1,32 @@
-import InventoryService from '#services/inventory.js' 
+import InventoryService from '#services/inventory.js'
 
 class InventoryController {
+    async listAll(_req, res) {
+        try {
+            const data = await InventoryService.listAll()
+            res.status(200).json(data)
+        } catch (error) {
+            res.status(500).json({ message: error.message })
+        }
+    }
 
-  async listById(req, res) {
-    try {
-      const data = await InventoryService.listById(req.params.id)
-      res.status(200).json(data)
-    } catch (error) {
-      res.status(500).json({ message: error.message })
-  }
-}
+    async listById(req, res) {
+        try {
+            const data = await InventoryService.listById(req.params.id)
+            res.status(200).json(data)
+        } catch (error) {
+            res.status(500).json({ message: error.message })
+        }
+    }
 
-async deleteById(req, res) {
-  try {
-      const data = await InventoryService.deleteById(req.params.id)
-      res.status(200).json(data)
-  } catch (error) {
-      res.status(500).json({message: error.message})
-  }
-}
-
+    async deleteById(req, res) {
+        try {
+            const data = await InventoryService.deleteById(req.params.id)
+            res.status(200).json(data)
+        } catch (error) {
+            res.status(500).json({ message: error.message })
+        }
+    }
 }
 
 export default new InventoryController()
--- src/models/inventory.js
+++ src/models/inventory.js
@@ -5,34 +5,29 @@ class Inventory {
         return database
             .from('inventory')
             .join('warehouse', 'inventory.warehouse_id', '=', 'warehouse.id')
-            .select('inventory.id', 'item_name', 'category', 'status', 
-            'quantity', 'warehouse.warehouse_name')
+            .select('inventory.id', 'item_name', 'category', 'status', 'quantity', 'warehouse.warehouse_name')
     }
 
-  listById(id) {
-    return database
-      .first()
-      .from('inventory')
-      .where('inventory.id', id)
-      .innerJoin('warehouse', 'warehouse.id', '=', 'inventory.warehouse_id')
-      .select(
-        'inventory.id',
-        'warehouse.warehouse_name',
-        'inventory.item_name',
-        'inventory.description',
-        'inventory.category',
-        'inventory.status',
-        'inventory.quantity'
-      )
-  }
-}
-
+    listById(id) {
+        return database
+            .first()
+            .from('inventory')
+            .where('inventory.id', id)
+            .innerJoin('warehouse', 'warehouse.id', '=', 'inventory.warehouse_id')
+            .select(
+                'inventory.id',
+                'warehouse.warehouse_name',
+                'inventory.item_name',
+                'inventory.description',
+                'inventory.category',
+                'inventory.status',
+                'inventory.quantity'
+            )
+    }
 
     deleteById(id) {
-        return database
-        .from('inventory')
-        .where('id', id)
-        .del()
+        return database.from('inventory').where('id', id).del()
+    }
 }
 
 export default new Inventory()
