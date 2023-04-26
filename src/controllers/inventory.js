5245b050327746c3a3d1bd61bd9962a8547beddd
Kaveri Mittal <114541367+Kaverim8@users.noreply.github.com>
2023-04-25 23:30:33 -0400

    feat: API to PUT/EDIT an Inventory Item

--- src/controllers/inventory.js
+++ src/controllers/inventory.js
@@ -36,6 +36,20 @@ class InventoryController {
             res.status(500).json({ message: error.message })
         }
     }
+
+    async updateById(req, res) {
+        try {
+            if (!req.body.item_name && !req.body.quantity) {
+                res.status(400).json({message: 'all are required'})
+            } else {
+                await InventoryService.updateById(req.params.id, req.body)
+                res.sendStatus(200)
+            }
+        } catch (error) {
+            res.status(500).json({message: error.message})
+        }
+    }
+
 }
 
 export default new InventoryController()
